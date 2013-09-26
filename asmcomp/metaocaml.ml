48b7dbb3ef33a86f3d7e2b4e49efce6df02513cc
Evgeny Roubinchtein <zhenya1007@gmail.com>
2013-09-25 23:31:36 -0700

    "Make the native standalone executables work."
    The problem was that, when Opttoploop.load_lambda called dll_run
    (which, in turn called caml_natdynlink_run_toplevel, which called
    caml_natdynlink_run), load_lambda was passing the string "TOP" to
    caml_natdynlink_run() as the name of the compilation unit (the second
    argument to caml_natdynlink_run(), i.e., "value symbol").  On the
    other hand, when load_lambda was compiling the shared library that was
    going to be passed to dll_run, the name of the compilation unit was
    the empty string (because the current value of
    Compilenv.current_unit_name() was the empty string).
    
    Running from inside opttopnat didn't have that problem because
    Opttoploop.execute_phrase calls Compilenv.reset.
    
    For now, I am just calling Compilenv.reset from metaocaml.ml.

--- asmcomp/metaocaml.ml
+++ asmcomp/metaocaml.ml
@@ -1,9 +1,23 @@
 let run_code s =
+  let dcmm = !Clflags.dump_cmm in
   let lam = (Marshal.from_string s 0 : Lambda.lambda) in
-  (* FIXME: always passing Format.std_formatter here is a kludge *)
-  (* FIXME: passing in 0 for size below is a kludge *)
-  match Opttoploop.load_lambda Format.std_formatter (0,lam) with
-  | Opttoploop.Result v -> v
-  | Opttoploop.Exception x -> raise x
+  try
+    (* FIXME: always passing Format.std_formatter here is a kludge *)
+    (* FIXME: passing in 0 for size below is a kludge *)
+    Clflags.dump_cmm := true;
+    (* FIXME: using the `magic string' "TOP" here enshrines a rather close knowledge
+       of the conventions used by opttoploop.ml *)
+    let n = Compilenv.current_unit_name () in
+    if String.length n > 2 && String.sub n 0 3 = "TOP" then () else Compilenv.reset ?packname:None "TOP";
+    let r =
+      match Opttoploop.load_lambda Format.std_formatter (0,lam) with
+      | Opttoploop.Result v -> v
+      | Opttoploop.Exception x -> raise x
+    in
+    Clflags.dump_cmm := dcmm;
+    r
+  with x ->
+    Clflags.dump_cmm := dcmm;
+    raise x
 
-let _ = Callback.register "Metaocaml.run_code" run_code
+let _ = Callback.register "Metaocaml.run_code" run_code;
